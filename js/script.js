b432a6e4f8561210b3cee3110f6ddf4c2db0acb6
WALM <lbhnik12@gmail.com>
2024-10-18 01:31:29 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":2056},"pages":[{"id":"page-menu","displayName":"Menu","link":{"linkType":"LinkTypePage","href":"#!page-menu"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
