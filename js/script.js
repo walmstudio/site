5b3ef9cb1a7a89c989c035f405d82d9726dc08f7
WALM <lbhnik12@gmail.com>
2025-01-04 16:20:35 +0400

    Delete js directory

--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-
-    window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":2056},"pages":[{"id":"page-menu","displayName":"Menu","link":{"linkType":"LinkTypePage","href":"#!page-menu"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
